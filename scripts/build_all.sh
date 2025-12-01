9f2b3b784b2136c293f3f09e794f893b22b35efc
HIP3ONE <eduardermakov769@gmail.com>
2025-12-01 21:13:25 +0800

    Update build_all.sh

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-# build_all.sh ONCODE
+# build_all.sh HIP3ONE
 # Build script for all components including smart contracts, backend, and frontend
 
 # Exit on any error to prevent partial builds
